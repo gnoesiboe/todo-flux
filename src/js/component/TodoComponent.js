c7344460a4c940f5e7720f0d3dfdd5afd30ab8e8
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2015-09-12 13:08:55 +0200

    #1 display edit form when clicking edit icon

--- src/js/component/TodoComponent.js
+++ src/js/component/TodoComponent.js
@@ -1,9 +1,21 @@
 var React = require('react'),
     AppDispatcher = require('./../dispatcher/AppDispatcher'),
     ActionFactory = require('./../action/ActionFactory'),
-    moment = require('moment');
+    moment = require('moment'),
+    modusConstants = require('./../constants/ModusConstants');
 
 var TodoComponent = React.createClass({
+
+    /**
+     * @returns {Object}
+     */
+    getInitialState: function () {
+        return {
+            title: this.props.todo.title,
+            date: this.props.todo.date,
+            modus: modusConstants.VIEW
+        };
+    },
 
     /**
      * Gets called when the checkbox state is changed
@@ -13,7 +25,7 @@ var TodoComponent = React.createClass({
     },
 
     /**
-     * GEts called when the user tries to delete a
+     * Gets called when the user tries to delete a
      */
     onDeleteClick: function () {
         var isConfirmed = confirm('Are you sure?');
@@ -23,6 +35,13 @@ var TodoComponent = React.createClass({
         }
     },
 
+    /**
+     * @param {String} date
+     *
+     * @returns {XML}
+     *
+     * @private
+     */
     _formatDate: function (date) {
         var className = "text-muted";
 
@@ -52,19 +71,87 @@ var TodoComponent = React.createClass({
     },
 
     /**
+     * @param {Object} event
+     */
+    onEditClick: function (event) {
+        event.preventDefault();
+
+        this.setState({
+            modus: modusConstants.EDIT
+        });
+    },
+
+    /**
+     * @returns {XML}
+     */
+    renderModusEdit: function () {
+        return (
+            <div>
+                <form className="form-inline" onSubmit={this.onFormSubmit}>
+                    <div className="form-group">
+                        <input className="form-control"
+                               type="text"
+                               ref="title"
+                               name="title"
+                               value={this.state.title}
+                               placeholder="title"/>&nbsp;
+                    </div>
+                    <div className="form-group">
+                        <input type="date"
+                               className="form-control"
+                               ref="date"
+                               value={this.state.date}
+                               name="date"/>&nbsp;
+                    </div>
+                    <button type="submit" className="btn btn-success">Save</button>
+                </form>
+            </div>
+        )
+    },
+
+    /**
+     * @returns {XML}
+     */
+    renderModusView: function () {
+        return (
+            <div className="checkbox">
+                <ul className="list-inline pull-right">
+                    <li><a href="#" onClick={this.onEditClick}>e</a></li>
+                    <li><a href="#" onClick={this.onDeleteClick}>x</a></li>
+                </ul>
+
+                <label>
+                    <input type="checkbox" refs="checkbox" checked={this.props.todo.isCompleted}
+                           onChange={this.handleOnCheckboxChange}/>
+                    {this._formatDate(this.props.todo.date)}&nbsp;-&nbsp;
+                    <strong>{this.props.todo.title}</strong>
+                </label>
+            </div>
+        )
+    },
+
+    /**
      * Renders this component into it's parent element
      *
      * @returns {XML}
      */
     render: function () {
+        var inner = null;
+
+        switch (this.state.modus) {
+            case modusConstants.EDIT:
+                inner = this.renderModusEdit();
+                break;
+
+            default:
+                inner = this.renderModusView();
+                break;
+        }
+
         return (
             <li className="list-group-item" ref="todo" data-id={this.props.todo.id}>
-                <div className="js-todo checkbox">
-                    <a href="#" className="pull-right" onClick={this.onDeleteClick}>x</a>
-                    <label>
-                        <input type="checkbox" refs="checkbox" checked={this.props.todo.isCompleted} onChange={this.handleOnCheckboxChange} />
-                        {this._formatDate(this.props.todo.date)}&nbsp;-&nbsp;<strong>{this.props.todo.title}</strong>
-                    </label>
+                <div className="js-todo">
+                    {inner}
                 </div>
             </li>
         );
